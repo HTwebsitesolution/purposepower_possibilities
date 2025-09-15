6e984e900754c5e96115af16fe8d5a70349ebc3a
HTwebsitesolution <htwebsitesolution@gmail.com>
2025-09-15 19:54:54 +0100

    Fix About page: Update with real images and clean up structure

---
+++ src/pages/About-test.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function AboutTest() {
+  return (
+    <div style={{padding: '20px', backgroundColor: 'white', color: 'black'}}>
+      <h1>About Joseph Afolabi - TEST PAGE</h1>
+      <p>This is a test version to check if routing works</p>
+      <img src="/images/author.jpg" alt="Author" style={{width: '200px', height: '200px', objectFit: 'cover'}} />
+    </div>
+  )
+}
---
+++ src/pages/SimpleTest.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function SimpleTest() {
+  return (
+    <div>
+      <h1>SIMPLE TEST - About Joseph Afolabi</h1>
+      <p>This is a simple test page.</p>
+      <img src="/images/author.jpg" alt="Test" style={{width: '200px'}} />
+    </div>
+  )
+}
